554eaa42e2c5d839ae735ddccc6e8bd76c605ce6
Aditya Garg <adityagargno1@gmail.com>
2024-12-06 21:43:08 +0530

    Updated project files for deployment

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 # setup.sh
-# Install Rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+# Install Rust, bypassing checks
+curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 
-# Set writable directories for Cargo and Rustup
+# Set the writable directories for Cargo and Rustup
 export CARGO_HOME=/tmp/cargo
 export RUSTUP_HOME=/tmp/rustup
 mkdir -p $CARGO_HOME $RUSTUP_HOME
+
+# Source the Rust environment variables
+source $HOME/.cargo/env
